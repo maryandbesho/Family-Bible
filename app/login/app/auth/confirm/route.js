836b6d613a67c46ee028eef7405a4e29b3e856d9
maryandbesho <maryandbesho@gmail.com>
2026-08-04 18:52:32 -0700

    Update route.js

--- app/login/app/auth/confirm/route.js
+++ app/login/app/auth/confirm/route.js
@@ -1,1 +1,19 @@
+import { createClient } from '@/lib/supabase/server'
+import { redirect } from 'next/navigation'
 
+export async function GET(request) {
+  const { searchParams } = new URL(request.url)
+  const token_hash = searchParams.get('token_hash')
+  const type = searchParams.get('type')
+  const next = searchParams.get('next') ?? '/'
+
+  if (token_hash && type) {
+    const supabase = await createClient()
+    const { error } = await supabase.auth.verifyOtp({ type, token_hash })
+    if (!error) {
+      redirect(next)
+    }
+  }
+
+  redirect('/login?error=Could not verify link')
+}
